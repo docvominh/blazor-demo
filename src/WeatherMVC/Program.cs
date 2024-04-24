5b5df37e1361df04e12f9bbf68a6a57a3bf4dc06
Minh Pham <phamducminh1990@gmail.com>
2024-04-24 09:18:09 +0700

    add mvc

---
+++ src/WeatherMVC/Program.cs
@@ -0,0 +1,38 @@
+using TheWeatherLibrary.Components.Weather.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddServerSideBlazor();
+builder.Services.AddControllersWithViews();
+
+
+var weatherHttpClient = new HttpClient { BaseAddress = new Uri("https://api.weatherapi.com") };
+builder.Services.AddSingleton(new WeatherApiClient(weatherHttpClient));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.MapBlazorHub();
+
+app.Run();
